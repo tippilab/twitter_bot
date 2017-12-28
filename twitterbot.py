b30d01c015135b4f3cb7deba7213a172211d360e
wasper256 <andremorhun@gmail.com>
2017-12-28 14:47:40 +0200

    add basic cache

--- twitterbot.py
+++ twitterbot.py
@@ -1,38 +1,48 @@
+import csv
 import os
 
-import csv
 import tweepy
 
-with open('followers.txt', 'r') as file:
-    list_users_url = []
-    reader = csv.reader(file)
-    for row in reader:
-        list_users_url.append(row[0])
 
-list_users = []
-for user_url in list_users_url:
-    if 'https://twitter.com/' in user_url:
-        list_users.append(user_url.split('https://twitter.com/')[-1])
+def main():
+    with open('followers.txt', 'r') as file:
+        list_users_url = []
+        reader = csv.reader(file)
+        for row in reader:
+            list_users_url.append(row[0])
+
+    list_users = []
+    for user_url in list_users_url:
+        if 'https://twitter.com/' in user_url:
+            list_users.append(user_url.split('https://twitter.com/')[-1])
+    consumer_key = os.environ.get('CONSUMER_KEY')
+    consumer_secret = os.environ.get('CONSUMER_SECRET')
+    access_token = os.environ.get('ACCESS_TOKEN')
+    access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(
+        auth, wait_on_rate_limit=True,
+        wait_on_rate_limit_notify=True,
+        retry_count=10, retry_delay=5,
+        retry_errors=5
+    )
+
+    with open('followers_cache.txt', 'r+') as f:
+        cached_users = []
+        reader = csv.reader(f)
+        for row in reader:
+            cached_users.append(row[0])
+        for user in list_users:
+            if ('https://twitter.com/' + user) not in cached_users:
+                print("Follow " + user)
+                api.create_friendship(user)
+                f.write('https://twitter.com/' + user + '\n')
+            else:
+                print("Skipped " + user + '\n')
 
 
-consumer_key = os.environ.get('CONSUMER_KEY')
-consumer_secret = os.environ.get('CONSUMER_SECRET')
-access_token = os.environ.get('ACCESS_TOKEN')
-access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(
-    auth, wait_on_rate_limit=True,
-    wait_on_rate_limit_notify=True,
-    retry_count=10, retry_delay=5,
-    retry_errors=5
-)
-
-
-with open('followers_cache.txt', 'a') as f:
-    for user in list_users:
-        print("Follow " + user)
-        api.create_friendship(user)
-        f.write('https://twitter.com/' + user + '\n')
+if __name__ == "__main__":
+    main()
