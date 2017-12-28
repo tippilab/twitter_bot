4be0617431fce32146204bbeced1bcf445d3ca3c
Sergey <sergeygorky@yahoo.com>
2017-12-28 11:11:24 +0200

    add os environ

--- twitterbot.py
+++ twitterbot.py
@@ -1,12 +1,13 @@
+import os
+
 import tweepy
-from random import randint
 
 list_users_url = [
-    'https://twitter.com/4gophers',
-    'https://twitter.com/zkonstantin',
-    'https://twitter.com/nmishin',
-    'https://twitter.com/iamstarkov',
-    'https://twitter.com/dcromster'
+    'https://twitter.com/hexlessons_ru',
+    'https://twitter.com/freetonik',
+    'https://twitter.com/htmlacademy_ru',
+    'https://twitter.com/JavaHexlet',
+    'https://twitter.com/hexlet_io'
 ]
 
 list_users = []
@@ -14,20 +15,20 @@ list_users = []
 for user_url in list_users_url:
     list_users.append(user_url.split('https://twitter.com/')[-1])
 
-consumer_key = 'your'
-consumer_secret = 'your'
-access_token = 'your'
-access_token_secret = 'your'
+consumer_key = os.environ.get('CONSUMER_KEY')
+consumer_secret = os.environ.get('CONSUMER_SECRET')
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(
-                auth, wait_on_rate_limit=True, 
-                wait_on_rate_limit_notify=True, 
-                retry_count=10, retry_delay=5, 
-                retry_errors=5
-                )
+    auth, wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True,
+    retry_count=10, retry_delay=5,
+    retry_errors=5
+)
 
 for user in list_users:
     print("Follow " + user)
