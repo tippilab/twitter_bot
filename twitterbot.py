7bb6369a245a9568bda9932dc1153f3d3b38d143
Sergey <sergeygorky@yahoo.com>
2017-12-28 13:17:47 +0200

    add configuration using ENV vars

--- twitterbot.py
+++ twitterbot.py
@@ -1,5 +1,7 @@
-import tweepy
+import os
 from random import randint
+
+import tweepy
 
 list_users_url = [
     'https://twitter.com/4gophers',
@@ -14,10 +16,10 @@ list_users = []
 for user_url in list_users_url:
     list_users.append(user_url.split('https://twitter.com/')[-1])
 
-consumer_key = 'your'
-consumer_secret = 'your'
-access_token = 'your'
-access_token_secret = 'your'
+consumer_key = os.environ.get('CONSUMER_KEY')
+consumer_secret = os.environ.get('CONSUMER_SECRET')
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
