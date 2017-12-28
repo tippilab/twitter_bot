db35d84e06cc988f4ca1cb0a65a29646eec12733
wasper256 <andremorhun@gmail.com>
2017-12-28 18:09:30 +0200

    add click, counter and some fixes

--- twitterbot.py
+++ twitterbot.py
@@ -2,9 +2,23 @@ import csv
 import os
 
 import tweepy
+import click
 
 
-def main():
+@click.command()
+@click.option('--consumer_key', default=os.environ.get('CONSUMER_KEY'),
+              help='CONSUMER_KEY')
+@click.option('--consumer_secret', default=os.environ.get('CONSUMER_SECRET'),
+              help='CONSUMER_KEY')
+@click.option('--access_token', default=os.environ.get('ACCESS_TOKEN'),
+              help='ACCESS_TOKEN')
+@click.option('--access_token_secret',
+              default=os.environ.get('ACCESS_TOKEN_SECRET'),
+              help='ACCESS_TOKEN_SECRET')
+@click.option('--limit', default=1000,
+              help='set limit amount of users you want to follow')
+def main(consumer_key, consumer_secret, access_token,
+         access_token_secret, limit):
     with open('followers.txt', 'r') as file:
         list_users_url = []
         reader = csv.reader(file)
@@ -15,10 +29,8 @@ def main():
     for user_url in list_users_url:
         if 'https://twitter.com/' in user_url:
             list_users.append(user_url.split('https://twitter.com/')[-1])
-    consumer_key = os.environ.get('CONSUMER_KEY')
-    consumer_secret = os.environ.get('CONSUMER_SECRET')
-    access_token = os.environ.get('ACCESS_TOKEN')
-    access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+        elif 'http://twitter.com/' in user_url:
+            list_users.append(user_url.split('http://twitter.com/')[-1])
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -29,17 +41,24 @@ def main():
         retry_count=10, retry_delay=5,
         retry_errors=5
     )
-
-    with open('followers_cache.txt', 'r+') as f:
+    print(api)
+    counter = 0
+    with open('followers_cache.txt', 'ar+') as f:
         cached_users = []
+        tmp_cache = []
         reader = csv.reader(f)
         for row in reader:
             cached_users.append(row[0])
         for user in list_users:
-            if ('https://twitter.com/' + user) not in cached_users:
+            if ('https://twitter.com/' + user) not in cached_users and (
+                    'https://twitter.com/' + user + '\n') not in tmp_cache:
                 print("Follow " + user)
                 api.create_friendship(user)
                 f.write('https://twitter.com/' + user + '\n')
+                tmp_cache.append('https://twitter.com/' + user + '\n')
+                counter += 1
+                if counter >= limit:
+                    return
             else:
                 print("Skipped " + user + '\n')
 
